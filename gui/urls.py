4477548f87b1f113fe9614e46e798f62ca39a50a
Darkikita <nikita.dakhno@gmx.de>
2025-09-08 09:25:11 +0200

    mehr webseiten

--- gui/urls.py
+++ gui/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from .views import individual
+from .views import site_view, start
 
 urlpatterns = [
-    path("", individual, name="individual"),
+    path("", start, name="start"),
+    path("<slug:slug>/", site_view, name="site-view"),
 ]
+
+
+# from .views import start, individual
+
+# urlpatterns = [
+#     path("", start, name="start"),
+#     path("individual/", individual, name="individual"),
+# ]
 
 # path("individual/", views.individual, name="individual"),
 # http://127.0.0.1:8000/individual/
--- gui/views.py
+++ gui/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
 
-def individual(request):
-    context = {"title": "Individual", "user": "Nikita"}
-    return render(request, "gui/individual.html", context)
+# Erlaube nur bekannte Vorlagen (Schutz vor Pfad-Traversal)
+ALLOWED_TEMPLATES = {
+    "start": "start/index.html",
+    "individual": "individual/index.html",
+    "character": "character/index.html",
+    "charity": "charity/index.html",
+    "loop": "loop/index.html",
+}
+
+def start(request):
+    return render(request, "start/index.html")
+
+def site_view(request, slug: str):
+    tpl = ALLOWED_TEMPLATES.get(slug)
+    if not tpl:
+        raise Http404("Unbekannte Vorlage")
+    return render(request, tpl, {"slug": slug})
 
 
-# Create your views here.
+### Standard
+# def start(request):
+#     return render(request, "gui/start.html")
+
+# def individual(request):
+#     return render(request, "gui/individual.html")
+###
+
